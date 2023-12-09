2394803f1bf1e86cec30a1378440ef37e381de71
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-12-09 12:36:52 +0530

    Update Upcoming.jsx

--- src/Components/Upcoming.jsx
+++ src/Components/Upcoming.jsx
@@ -11,13 +11,13 @@ function Upcoming({rendered}) {
         if(!isSearch && rendered === 'upcoming'){
             return upcomingAnime?.map((anime) => {
                 return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="Anime Image" />
+                    <img src={anime.images.webp.large_image_url} alt="Anime Image" />
                 </Link>
             })
         }else{
             return searchResults?.map((anime) => {
                 return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="Anime Image" />
+                    <img src={anime.images.webp.large_image_url} alt="Anime Image" />
                 </Link>
             })
         }
