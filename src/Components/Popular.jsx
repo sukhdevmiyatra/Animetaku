dfe2ea9d052e393052a41d656a46e613c9bdae0c
sukhdevmiyatra <141604120+sukhdevmiyatra@users.noreply.github.com>
2023-12-09 12:40:38 +0530

    Update Popular.jsx

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useGlobalContext } from '../context/global'
 import styled from 'styled-components'
-import Sidebar from './Sidebar'
+
 
 function Popular({rendered}) {
     const {popularAnime,isSearch, searchResults} = useGlobalContext()
